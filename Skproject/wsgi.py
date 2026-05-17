eed148233d766b893182d79f8db40f480fa7b266
RAMAN SINGH RAGHAV <2203051240081@paruluniversity.ac.in>
2026-05-17 09:10:56 +0530

    fix: correct DJANGO_SETTINGS_MODULE in wsgi.py

--- Skproject/wsgi.py
+++ Skproject/wsgi.py
@@ -1,16 +1,7 @@
+﻿"""
+WSGI config for Skproject project.
 """
-WSGI config for NewDjangoProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewDjangoProject.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Skproject.settings')
 application = get_wsgi_application()
